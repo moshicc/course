845111371ab7a972bc4ff4e519165fac44ed93e0
张聪聪 <1316435855@qq.com>
2020-04-28 14:54:20 +0800

    1.新建公共模块server

---
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -0,0 +1,15 @@
+package com.course.server.mapper;
+
+import com.course.server.domain.Test;
+
+import java.util.List;
+
+/**
+ * @author zcc
+ * @date 2020/4/28 13:35
+ * @description
+ */
+
+public interface TestMapper {
+    public List<Test> list();
+}
