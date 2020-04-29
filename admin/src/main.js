a50e3e7b00d6e11c9da100880aaae2f5ef1d9aef
张聪聪 <1316435855@qq.com>
2020-04-29 10:40:04 +0800

    1.增加login页面 2.增加路由配置，访问/login时，加载login组件

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 //$mount 意思大概是渲染展示，展示当前目录下的APP.vue,渲染到#app。就是把APP.vue中的<template> 替换掉index页面中的id=app标签
 new Vue({
+    router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
