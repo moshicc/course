9394c917f39c55f5cc215c4a3af7d37b98902c1e
张聪聪 <1316435855@qq.com>
2020-05-06 22:19:59 +0800

    1.集成axios完成前后端交互 2.增加CorsConfig ，解决前后端跨域的问题

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false;
+Vue.prototype.$ajax =axios;
 //$mount 意思大概是渲染展示，展示当前目录下的APP.vue,渲染到#app。就是把APP.vue中的<template> 替换掉index页面中的id=app标签
 new Vue({
     router,
--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -15,13 +15,13 @@ import java.util.List;
  * @description
  */
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/chapter")
 public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
-    @RequestMapping("/chapter")
-    public List<ChapterDto> chapter(){
+    @RequestMapping("/list")
+    public List<ChapterDto> list(){
         return chapterService.list();
     }
 }
