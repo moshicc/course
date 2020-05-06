99efa7502fe473b41bb6253032d131f300a4fede
张聪聪 <1316435855@qq.com>
2020-05-06 21:53:27 +0800

    1.通用的sidebar点击激活样式方法，使用watch监听路由变化

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -474,8 +474,23 @@
     export default {
         name: "admin",
         mounted:function(){
+            let _this =this;
             $("body").removeClass("login-layout light-login");
             $("body").attr("class", "no-skin");
+            //sidebar激活样式 方法二
+            _this.activeSidebar(_this.$route.name.replace("/","-")+"sidebar");
+
+        },watch: {
+          $route: {
+              handler:function (val,oldVal) {
+                  //sidebar激活样式 方法二
+                  console.log("----->页面跳转：",val,oldVal);
+                  let _this =this;
+                  _this.$nextTick(function () {//页面加载完毕后执行
+                      _this.activeSidebar(_this.$route.name.replace("/","-")+"sidebar");
+                  })
+              }
+          }
         },
         methods:{
             login(){
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -976,7 +976,8 @@ Submit
     export default {
         name: "chapter",
         mounted:function(){
-            this.$parent.activeSidebar("business-chapter-sidebar");
+            //sidebar激活样式 方法一
+            //this.$parent.activeSidebar("business-chapter-sidebar");
         },
         methods:{
         }
