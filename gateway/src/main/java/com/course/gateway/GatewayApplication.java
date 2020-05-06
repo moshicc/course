47ea1765bc3ab0527dc63702d674be6a05025370
张聪聪 <1316435855@qq.com>
2020-05-06 23:04:50 +0800

    1.解决gateway跨域问题， 2.修复前端页面js文件找不到问题

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,6 +22,22 @@ public static void main(String[] args) {
     Environment env = app.run(args).getEnvironment();
     LOG.info("启动成功！！");
     LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+}
+/**
+ * 配置跨域
+ */
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**",config);
+        return new CorsWebFilter(source);
 }
 
 }
