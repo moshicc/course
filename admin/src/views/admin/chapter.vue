e3456865ac64735e97c955cbff1470086a0755a6
张聪聪 <1316435855@qq.com>
2020-05-12 18:39:58 +0800

    1.增加工具类tool.js 和校验类validaton.js 2.大章保存非空和长度增加校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -124,6 +124,13 @@
 
             save(page) {
                 let _this =this;
+                //保存校验
+                if(!Validator.require(_this.chapter.name,"名称")
+                    ||!Validator.require(_this.chapter.courseId,"课程ID")
+                    ||!Validator.length(_this.chapter.courseId,"课程ID",1,8 )){
+                    return;
+                }
+
                 Loading.show();
                 _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/save", _this.chapter).then((resopnes)=>{
                     Loading.hide();
@@ -154,29 +161,6 @@
                         }
                     })
                 });
-                // Swal.fire({
-                //     title: '确认删除？',
-                //     text: "删除后不可恢复，确认删除？",
-                //     icon: 'warning',
-                //     showCancelButton: true,
-                //     confirmButtonColor: '#3085d6',
-                //     cancelButtonColor: '#d33',
-                //     confirmButtonText: '确认!'
-                // }).then((result) => {
-                //     if (result.value) {
-                //         Loading.show();
-                //         _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/"+id).then((resopnes)=>{
-                //             Loading.hide();
-                //             console.log("删除大章列表结果：",resopnes);
-                //             let resp = resopnes.data;
-                //             if(resp.success){
-                //                 //删除成功，并且刷新list
-                //                 _this.list(1);
-                //                 Toast.success("删除成功！");
-                //             }
-                //         })
-                //     }
-                // })
 
             }
         }
