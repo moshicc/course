e6217f4585ff4c5b0d57fd81b7fd1fc3b5ad2a86
张聪聪 <1316435855@qq.com>
2020-05-12 17:34:35 +0800

    1.集成sweetalert2，删除时弹出确认框

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -135,15 +135,33 @@
             },
             del(id) {
                 let _this =this;
-                _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/"+id).then((resopnes)=>{
-                    console.log("删除大章列表结果：",resopnes);
-                    let resp = resopnes.data;
-                    if(resp.success){
-                        //删除成功，并且刷新list
-                        _this.list(1)
+                //sweetalert弹出框
+                Swal.fire({
+                    title: '确认删除？',
+                    text: "删除后不可恢复，确认删除？",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: '确认!'
+                }).then((result) => {
+                    if (result.value) {
+                        _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/"+id).then((resopnes)=>{
+                            console.log("删除大章列表结果：",resopnes);
+                            let resp = resopnes.data;
+                            if(resp.success){
+                                //删除成功，并且刷新list
+                                _this.list(1);
+                                Swal.fire(
+                                    '删除成功',
+                                    '删除成功.',
+                                    'success'
+                                )
+                            }
+                        })
                     }
-
                 })
+
             }
         }
     }
