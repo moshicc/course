ed9dcfdb21aee065684fcc9d2d79ba9bc95da6d1
张聪聪 <1316435855@qq.com>
2020-05-13 18:11:42 +0800

    1.生成器开发：解决了controller模板中的 中文业务名称和模板名称问题

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -15,16 +15,21 @@ import java.util.Map;
 
     public class ServerGenerator {
 
+    static String MODULE ="business";
     static String toServicePath = "server\\src\\main\\java\\com\\course\\server\\service\\";
-    static String toControllerPath = "business\\src\\main\\java\\com\\course\\business\\controller\\admin\\";
+    static String toControllerPath = MODULE+"\\src\\main\\java\\com\\course\\"+MODULE+"\\controller\\admin\\";
 
     public static void main(String[] args) throws IOException, TemplateException {
 
         String Domain = "Section";
+        String tableNameCn ="小节";
         String domain = "section";
+        String module =MODULE;
         Map<String ,Object> map = new HashMap<>();
         map.put("Domain",Domain);
         map.put("domain",domain);
+        map.put("tableNameCn",tableNameCn);
+        map.put("module",module);
 
         //生成service
         FreemarkerUtil.initConfig("service.ftl");
