9aa54037860ea81ec823bc9e1d86be04f3f149ef
张聪聪 <1316435855@qq.com>
2020-05-19 18:29:21 +0800

    1.增加了枚举常量

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -134,7 +134,7 @@
             return {
                 section: {},
                 sections: [],
-                CHARGE:[{key:"C",value:"收费"},{key:"F",value:"免费"}],
+                CHARGE:CHARGE,
             }
         },
         mounted:function(){
